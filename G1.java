c1dbaf4ae9e81e07786e522358d1b134f94103ef
Poojashelar1803 <poojashelar8767@gmail.com>
2025-08-01 09:38:18 +0530

    Add files via upload

---
+++ G1.java
@@ -0,0 +1,17 @@
+class G1{
+String name;
+int id;
+G1(String name,int id){
+this.name=name;
+this.id=id;
+}
+}
+class GFG
+{
+public static void main(String[] args)
+{
+G1  geeks =new G1("Shweta",68);
+System.out.println("G1Name:" +geeks.name +"and G1id:"+geeks.id);
+}
+
+} 
