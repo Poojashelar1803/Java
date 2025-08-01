5280c4320ee0c6a79f79c90926b3be89d0539b65
Poojashelar1803 <poojashelar8767@gmail.com>
2025-08-01 09:52:06 +0530

    Add files via upload

---
+++ Slip7A.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+
+import java.awt.event.*;
+
+ 
+
+public class Slip7A extends Frame{
+
+    public void paint(Graphics g){
+
+        Font f = new Font("Georgia",Font.PLAIN,20);
+
+        g.setFont(f);
+
+        g.drawString("Dr D Y Patil College", 50, 70);
+
+        setBackground(Color.RED);
+
+    }
+
+    public static void main(String args[]){
+
+        Slip7A sl = new Slip7A();
+
+        sl.setVisible(true);
+
+        sl.setSize(200,300);
+
+    }
+
+
+}
